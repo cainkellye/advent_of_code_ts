37251387deeccbf4fd72056c5444f072cdbccd21
Laszlo Lenkei <laszlo.lenkei.ext@siemens-healthineers.com>
2025-09-15 19:46:08 +0200

    2019/day01

--- 2019/day01.ts
+++ 2019/day01.ts
@@ -2,13 +2,17 @@ import { runDay, log } from "../utils/helpers.ts";
 import * as F from "../utils/functional.ts";
 
 export function part1(input: string[]): number {
-    // Example: count lines
-    return input.filter(l => l).length;
+    return input.map(line => Math.floor(parseInt(line) / 3) - 2).reduce((x, y) => x + y);
 }
 
 export function part2(input: string[]): number {
-  // Example: sum line lengths using functional utility
-  return F.sum(input.map(line => line.length));
+    return F.sum(input.map(line => fuelCost(parseInt(line))));
+}
+
+function fuelCost(mass: number): number {
+    const fuel = Math.floor(mass / 3) - 2;
+    if (fuel <= 0) return 0;
+    return fuel + fuelCost(fuel);
 }
 
 // To run this day directly
